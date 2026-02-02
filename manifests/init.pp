4ba5dbdae0883dbe91f10dd6cac535eea5a5c855
Chris Allen <chris.j.allen1979@gmail.com>
2026-02-02 11:26:01 +0000

    Automated conversion from geerlingguy.redis Ansible role

--- manifests/init.pp
+++ manifests/init.pp
@@ -28,6 +28,7 @@
 # @param redis_save Set to an empty set to disable persistence (saving the DB to disk).
 # @param redis_includes Add extra include files for local configuration/overrides.
 # @param redis_disabled_commands Disable certain Redis commands for security reasons.
+# @param par_vardir Base directory for Puppet agent cache (uses lookup('paw::par_vardir') for common config)
 # @param par_tags An array of Ansible tags to execute (optional)
 # @param par_skip_tags An array of Ansible tags to skip (optional)
 # @param par_start_at_task The name of the task to start execution at (optional)
@@ -67,6 +68,7 @@ class paw_ansible_role_redis (
   Array $redis_save = ['900 1', '300 10', '60 10000'],
   Array $redis_includes = [],
   Array $redis_disabled_commands = [],
+  Optional[Stdlib::Absolutepath] $par_vardir = undef,
   Optional[Array[String]] $par_tags = undef,
   Optional[Array[String]] $par_skip_tags = undef,
   Optional[String] $par_start_at_task = undef,
@@ -80,14 +82,13 @@ class paw_ansible_role_redis (
   Optional[Boolean] $par_exclusive = undef
 ) {
 # Execute the Ansible role using PAR (Puppet Ansible Runner)
-  $vardir = $facts['puppet_vardir'] ? {
-    undef   => $settings::vardir ? {
-      undef   => '/opt/puppetlabs/puppet/cache',
-      default => $settings::vardir,
-    },
-    default => $facts['puppet_vardir'],
+# Playbook synced via pluginsync to agent's cache directory
+# Check for common paw::par_vardir setting, then module-specific, then default
+  $_par_vardir = $par_vardir ? {
+    undef   => lookup('paw::par_vardir', Stdlib::Absolutepath, 'first', '/opt/puppetlabs/puppet/cache'),
+    default => $par_vardir,
   }
-  $playbook_path = "${vardir}/lib/puppet_x/ansible_modules/ansible_role_redis/playbook.yml"
+  $playbook_path = "${_par_vardir}/lib/puppet_x/ansible_modules/ansible_role_redis/playbook.yml"
 
   par { 'paw_ansible_role_redis-main':
     ensure        => present,
